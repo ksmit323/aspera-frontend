80f84ba2b8bb4591dfce7014d7463703af367b94
Ubuntu <ubuntu@ip-172-31-1-14.ap-southeast-2.compute.internal>
2026-04-07 01:22:32 +0000

    fix: simplify Web3Provider to use default config

--- src/lib/Web3Provider.tsx
+++ src/lib/Web3Provider.tsx
@@ -1,36 +1,17 @@
 'use client'
 
-import { RainbowKitProvider, getDefaultConfig, connectorsForWallets } from '@rainbow-me/rainbowkit'
-import { metaMaskWallet, walletConnectWallet, trustWallet, okxWallet, coinbaseWallet } from '@rainbow-me/rainbowkit/wallets'
+import { RainbowKitProvider, getDefaultConfig } from '@rainbow-me/rainbowkit'
 import { WagmiProvider } from 'wagmi'
 import { mainnet, polygon, optimism, arbitrum, base } from 'wagmi/chains'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const WALLET_CONNECT_PROJECT_ID = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || ''
 
-const connectors = connectorsForWallets(
-  [
-    {
-      groupName: 'Popular',
-      wallets: [metaMaskWallet, coinbaseWallet],
-    },
-    {
-      groupName: 'Mobile & QR',
-      wallets: [walletConnectWallet, trustWallet, okxWallet],
-    },
-  ],
-  {
-    appName: 'Aspera',
-    projectId: WALLET_CONNECT_PROJECT_ID,
-  }
-)
-
 const config = getDefaultConfig({
   appName: 'Aspera',
   projectId: WALLET_CONNECT_PROJECT_ID,
   chains: [mainnet, polygon, optimism, arbitrum, base],
   ssr: true,
-  connectors,
 })
 
 const queryClient = new QueryClient()
